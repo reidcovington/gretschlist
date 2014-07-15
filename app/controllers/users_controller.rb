6eed987e0cef93d36609f0f3b20b66d4465b5ce3
Reid Covington <reid@shopcube.com>
2014-07-15 18:22:06 -0400

    add user create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,14 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:error] = "Unable to create user."
+      redirect_to root_path
+    end
   end
 
   def show
@@ -19,5 +27,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
